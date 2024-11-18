9cb29fbb0abef70b13f32e40263c95d6139f91b3
Suhani Mathur <139855595+suhani2706@users.noreply.github.com>
2024-11-18 12:52:59 +0530

    Added files of binary search tree assignment

---
+++ Constants.java
@@ -0,0 +1,17 @@
+package dsa;
+
+public class Constants {
+	
+	public static final String MENU = "Choose an option:";
+	public static final String MENU_OPTION_1 = "1. Insert a new node/Create a new BST ";
+	public static final String MENU_OPTION_2 = "2. Change root node";
+	public static final String MENU_OPTION_3 = "3. Print BST";
+	public static final String MENU_OPTION_4 = "4. Exit";
+	public static final String NODE_INPUT = "Enter node values one by one (type 'exit' to stop):";
+	public static final String AFTER_INSERTION = "BST Structure after insertion of ";
+	public static final String NEW_NODE = "Enter the new root node value:";
+	public static final String EXITING = "Exiting...";
+	public static final String NEW_BST = "BST Structure after changing root to ";
+   	
+	
+}
